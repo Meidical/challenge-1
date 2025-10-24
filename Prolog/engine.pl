10f9f0064fa5e3247017c8103e70f3700c3fde1e
TomasLPereira <1210830@isep.ipp.pt>
2025-10-24 01:36:07 +0100

    Added support for multiple concurrent patients in Prolog

--- Prolog/engine.pl
+++ Prolog/engine.pl
@@ -1,184 +1,188 @@
-:-op(220,xfx,entao).
-:-op(35,xfy,se).
-:-op(240,fx,regra).
-:-op(500,fy,nao).
-:-op(600,xfy,e).
+:- op(220, xfx, entao).
+:- op(35, xfy, se).
+:- op(240, fx, regra).
+:- op(500, fy, nao).
+:- op(600, xfy, e).
 
-:-dynamic justifica/3.
+:- dynamic facto/3.
+:- dynamic justifica/4.
+:- dynamic ultimo_facto/2.
 
-arranca_motor:-
-	findall(_,arranca_motor1,_).
+%%%%%%%%%%%%%%%%%%%%%%%%
+% Motor de inferência  %
+%%%%%%%%%%%%%%%%%%%%%%%%
 
-arranca_motor1:-
-    facto(N,Facto),
-    contar_factos(Cont),
-    assertz(ultimo_facto(Cont)),
-    facto_dispara_regras1(Facto, LRegras),
-    dispara_regras(N, Facto, LRegras),
-    retractall(ultimo_facto(_)).
+arranca_motor(PatientID) :-
+    forall(
+        facto(PatientID, N, Facto),
+        arranca_motor1(PatientID, N, Facto)
+    ).
 
-contar_factos(Cont):-
-    findall(X, facto(X, _), Lista),
+arranca_motor1(PatientID, N, Facto) :-
+    contar_factos(PatientID, Cont),
+    retractall(ultimo_facto(PatientID, _)),
+    assertz(ultimo_facto(PatientID, Cont)),
+    facto_dispara_regras1(PatientID, Facto, LRegras),
+    dispara_regras(PatientID, N, Facto, LRegras).
+
+contar_factos(PatientID, Cont) :-
+    findall(X, facto(PatientID, X, _), Lista),
     length(Lista, Cont).
 
-prox_facto(N) :-
-    contar_factos(Cont),
+prox_facto(PatientID, N) :-
+    contar_factos(PatientID, Cont),
     N is Cont + 1.
 
-facto_dispara_regras1(Facto, LRegras):-
-	facto_dispara_regras(Facto, LRegras),
-	!.
-facto_dispara_regras1(_, []).
-% Caso em que o facto não origina o disparo de qualquer regra.
+facto_dispara_regras1(_, Facto, LRegras) :-
+    facto_dispara_regras(Facto, LRegras),
+    !.
+facto_dispara_regras1(_, _, []).
 
-dispara_regras(N, Facto, [ID|LRegras]):-
-	regra ID se LHS entao RHS,
-	facto_esta_numa_condicao(Facto,LHS),
-	% Instancia Facto em LHS
-	verifica_condicoes(LHS, LFactos),
-	member(N,LFactos),
-	concluir(RHS,ID,LFactos),
-	!,
-	dispara_regras(N, Facto, LRegras).
+dispara_regras(PatientID, N, Facto, [ID|LRegras]) :-
+    regra ID se LHS entao RHS,
+    facto_esta_numa_condicao(Facto, LHS),
+    verifica_condicoes(PatientID, LHS, LFactos),
+    member(N, LFactos),
+    concluir(PatientID, RHS, ID, LFactos),
+    !,
+    dispara_regras(PatientID, N, Facto, LRegras).
 
-dispara_regras(N, Facto, [_|LRegras]):-
-	dispara_regras(N, Facto, LRegras).
-
-dispara_regras(_, _, []).
+dispara_regras(PatientID, N, Facto, [_|LRegras]) :-
+    dispara_regras(PatientID, N, Facto, LRegras).
+dispara_regras(_, _, _, []).
 
 
-facto_esta_numa_condicao(F,[F  e _]).
-
-facto_esta_numa_condicao(F,[avalia(F1)  e _]):- F=..[H,H1|_],F1=..[H,H1|_].
-
-facto_esta_numa_condicao(F,[_ e Fs]):- facto_esta_numa_condicao(F,[Fs]).
-
+facto_esta_numa_condicao(F,[F e _]).
+facto_esta_numa_condicao(F, [avalia(F1) e _]) :-
+    F =.. [Functor, Arg1 | _],
+    F1 =.. [Functor, Arg1 | _].
+facto_esta_numa_condicao(F,[_ e Fs]) :-
+    facto_esta_numa_condicao(F,[Fs]).
 facto_esta_numa_condicao(F,[F]).
+facto_esta_numa_condicao(F, [avalia(F1)]) :-
+    F =.. [Functor, Arg1 | _],
+    F1 =.. [Functor, Arg1 | _].
 
-facto_esta_numa_condicao(F,[avalia(F1)]):-F=..[H,H1|_],F1=..[H,H1|_].
+verifica_condicoes(PatientID, [nao avalia(X) e Y],[nao X|LF]) :- !,
+    \+ avalia(PatientID, _, X),
+    verifica_condicoes(PatientID, [Y], LF).
+verifica_condicoes(PatientID, [avalia(X) e Y],[N|LF]) :- !,
+    avalia(PatientID, N, X),
+    verifica_condicoes(PatientID, [Y], LF).
 
+verifica_condicoes(PatientID, [nao avalia(X)],[nao X]) :- !,
+    \+ avalia(PatientID, _, X).
+verifica_condicoes(PatientID, [avalia(X)],[N]) :- !,
+    avalia(PatientID, N, X).
 
-verifica_condicoes([nao avalia(X) e Y],[nao X|LF]):- !,
-	\+ avalia(_,X),
-	verifica_condicoes([Y],LF).
-verifica_condicoes([avalia(X) e Y],[N|LF]):- !,
-	avalia(N,X),
-	verifica_condicoes([Y],LF).
+verifica_condicoes(PatientID, [nao X e Y],[nao X|LF]) :- !,
+    \+ facto(PatientID, _, X),
+    verifica_condicoes(PatientID, [Y], LF).
+verifica_condicoes(PatientID, [X e Y],[N|LF]) :- !,
+    facto(PatientID, N, X),
+    verifica_condicoes(PatientID, [Y], LF).
 
-verifica_condicoes([nao avalia(X)],[nao X]):- !, \+ avalia(_,X).
-verifica_condicoes([avalia(X)],[N]):- !, avalia(N,X).
-
-verifica_condicoes([nao X e Y],[nao X|LF]):- !,
-	\+ facto(_,X),
-	verifica_condicoes([Y],LF).
-verifica_condicoes([X e Y],[N|LF]):- !,
-	facto(N,X),
-	verifica_condicoes([Y],LF).
-
-verifica_condicoes([nao X],[nao X]):- !, \+ facto(_,X).
-verifica_condicoes([X],[N]):- facto(N,X).
-
-
-
-concluir([cria_facto(F)|Y],ID,LFactos):-
-	!,
-	cria_facto(F,ID,LFactos),
-	concluir(Y,ID,LFactos).
-
-concluir([],_,_):-!.
+verifica_condicoes(PatientID, [nao X],[nao X]) :- !,
+    \+ facto(PatientID, _, X).
+verifica_condicoes(PatientID, [X],[N]) :-
+    facto(PatientID, N, X).
 
 
+concluir(PatientID, [cria_facto(F)|Y], ID, LFactos) :- !,
+    cria_facto(PatientID, F, ID, LFactos),
+    concluir(PatientID, Y, ID, LFactos).
+concluir(_, [], _, _) :- !.
 
-cria_facto(F,_,_):-
-	facto(_,F),!.
-
-cria_facto(F,ID,LFactos):-
-    retract(ultimo_facto(N1)),
-    N is N1+1,
-    asserta(ultimo_facto(N)),
-    assertz(justifica(N,ID,LFactos)),
-    assertz(facto(N,F)).
-    %format('Foi concluído o facto nº ~w -> ~w~n', [N, F]),!.
-
-
-avalia(N,P):-
-    P=..[Functor,Entidade,Operando,Valor],
-    P1=..[Functor,Entidade,Valor1],
-    facto(N,P1),
-    compara(Valor1,Operando,Valor).
-
-compara(V1,==,V):- V1==V.
-compara(V1,\==,V):- V1\==V.
-compara(V1,>,V):-V1>V.
-compara(V1,<,V):-V1<V.
-compara(V1,>=,V):-V1>=V.
-compara(V1,=<,V):-V1=<V.
+cria_facto(PatientID, F, _, _) :-
+    facto(PatientID, _, F), !.
+cria_facto(PatientID, F, ID, LFactos) :-
+    prox_facto(PatientID, N),
+    assertz(justifica(PatientID, N, ID, LFactos)),
+    assertz(facto(PatientID, N, F)).
 
 
+avalia(PatientID, N, P) :-
+    % Case 1: comparison on a 2-arg fact like mnemonica_cf("LEMON", >, 0.3)
+    P =.. [Functor, Entidade, Operador, Valor],
+    P1 =.. [Functor, Entidade, Valor1],
+    facto(PatientID, N, P1),
+    
+    compara(Valor1, Operador, Valor), !.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Inferir probabilidade de via aérea difícil %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-inferir_via_aerea(Dict) :-         
-    retractall(facto(_,_)),                      
-    retractall(ultimo_facto(_)),
+compara(V1, ==, V)  :- V1 == V.
+compara(V1, \==, V) :- V1 \== V.
+compara(V1, >, V)   :- V1 > V.
+compara(V1, <, V)   :- V1 < V.
+compara(V1, >=, V)  :- V1 >= V.
+compara(V1, =<, V)  :- V1 =< V.
 
-    assertz(facto(1, id_paciente(Dict.patientId))),
-    assertz(facto(2, idade(Dict.age))),
-    assertz(facto(3, bmi(Dict.bmi))),
-    assertz(facto(4, mnemonica("LEMON", 0.5))),
-    assertz(facto(5, mnemonica("MOANS", 0.2))),
-    assertz(facto(6, mnemonica("RODS", 0.2))),
-    assertz(facto(7, mnemonica("SHORT", 0.1))),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Avaliação de via aérea  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    asserta(ultimo_facto(7)), 
+inferir_via_aerea(Dict) :-
+    get_dict(patientId, Dict, PatientID),
+    retractall(facto(PatientID, _, _)),
+    retractall(ultimo_facto(PatientID, _)),
 
-    assert_lista_fatores(Dict.lemonFactors),
-    assert_lista_fatores(Dict.moansFactors),
-    assert_lista_fatores(Dict.rodsFactors),
-    assert_lista_fatores(Dict.shortFactors),
+    get_dict(age, Dict, Age),
+    prox_facto(PatientID, N1),
+    assertz(facto(PatientID, N1, idade(Age))),
 
-    arranca_motor,
+    get_dict(bmi, Dict, BMI),
+    prox_facto(PatientID, N2),
+    assertz(facto(PatientID, N2, bmi(BMI))),
+
+    prox_facto(PatientID, N3),
+    assertz(facto(PatientID, N3, mnemonica("LEMON", 0.5))),
+    prox_facto(PatientID, N4),
+    assertz(facto(PatientID, N4, mnemonica("MOANS", 0.2))),
+    prox_facto(PatientID, N5),
+    assertz(facto(PatientID, N5, mnemonica("RODS", 0.2))),
+    prox_facto(PatientID, N6),
+    assertz(facto(PatientID, N6, mnemonica("SHORT", 0.1))),
+
+    assert_lista_fatores(PatientID, Dict.lemonFactors),
+    assert_lista_fatores(PatientID, Dict.moansFactors),
+    assert_lista_fatores(PatientID, Dict.rodsFactors),
+    assert_lista_fatores(PatientID, Dict.shortFactors),
+
+    arranca_motor(PatientID),
 
     forall(
-        facto(_, mnemonica(Nome, Peso)),
-        (
-            calcular_cf(Nome, CF),
+        facto(PatientID, _, mnemonica(Nome, Peso)),
+        (   calcular_cf(PatientID, Nome, CF),
             CF1 is CF * Peso,
-            prox_facto(N),
-            assertz(facto(N, mnemonica_cf(Nome, CF1)))
+            prox_facto(PatientID, N7),
+            assertz(facto(PatientID, N7, mnemonica_cf(Nome, CF1)))
         )
     ),
 
-    arranca_motor.
+    arranca_motor(PatientID).
 
+assert_lista_fatores(_, null) :- !.
+assert_lista_fatores(_, []) :- !.
+assert_lista_fatores(PatientID, [H|T]) :-
+    (H.present == true ->
+        assert_fator(PatientID, H.category, H.code)
+    ; true),
+    assert_lista_fatores(PatientID, T).
 
-assert_lista_fatores(null) :- !. 
-assert_lista_fatores([]) :- !.        
-assert_lista_fatores([H | Lista]) :-
-    (H.present == true, assert_fator(H.category, H.code);   
-    H.present \== true),
-    assert_lista_fatores(Lista).       
-
-
-assert_fator(Category, Code) :-
-    (retract(ultimo_facto(N1)) -> true ; N1 = 0),
-    N is N1 + 1,
-    asserta(ultimo_facto(N)),
-    assertz(facto(N, fator(Category, Code))).
-
+assert_fator(PatientID, Category, Code) :-
+    prox_facto(PatientID, N),
+    assertz(facto(PatientID, N, fator(Category, Code))).
 
 %%%%%%%%%%%%%%%%%%%%
-% Via aérea normal %
+% Processos 
 %%%%%%%%%%%%%%%%%%%%
 
-laringoscopia(Dict) :-
-    prox_facto(N),
-    assertz(facto(N, processo("LD", Dict.successful))),
-    arranca_motor.
+laringoscopia(PatientID, Dict) :-
+    prox_facto(PatientID, N),
+    assertz(facto(PatientID, N, processo("LD", Dict.successful))),
+    arranca_motor(PatientID).
 
-mascara_facial(Dict) :-
-    prox_facto(N),
-    assertz(facto(N, processo("MF", Dict.successful))),
-    arranca_motor.
+mascara_facial(PatientID, Dict) :-
+    prox_facto(PatientID, N),
+    assertz(facto(PatientID, N, processo("MF", Dict.successful))),
+    arranca_motor(PatientID).
--- Prolog/http_handlers.pl
+++ Prolog/http_handlers.pl
@@ -1,46 +1,59 @@
-% HTTP GET para obter factos
-:- http_handler(root(show), get_facts_json, []).
+% HTTP GET para obter factos do paciente
+:- http_handler(root(PatientID/facts), get_facts_json(PatientID), []).
+
+get_facts_json(PatientID, _Request) :-
+    mostra_factos_json(PatientID, JSON),
+    reply_json(JSON).
+
+% HTTP GET para obter todos os factos
+:- http_handler(root(facts), get_facts_json, []).
 get_facts_json(_Request) :-
     mostra_factos_json(JSON),
     reply_json(JSON).
 
 % HTTP POST para inferir probabilidade de via aérea difícil
-:- http_handler(root(inferir_via_aerea), post_inferir_via_aerea, [method(post)]).
+:- http_handler(root(assessment), post_inferir_via_aerea, [method(post)]).
+
 post_inferir_via_aerea(Request) :-
     http_read_json_dict(Request, Dict),
     inferir_via_aerea(Dict),
-    build_inferir_via_aerea_json(JSONFinal),
+    PatientID = Dict.patientId,
+    build_inferir_via_aerea_json(PatientID, JSONFinal),
     reply_json(json(JSONFinal)).
 
-build_inferir_via_aerea_json(JSONFinal) :-
-    findall(Name=CF, facto(_, mnemonica_cf(Name, CF)), JSON1),
+build_inferir_via_aerea_json(PatientID, JSONFinal) :-
+    findall(Name=CF, facto(PatientID, _, mnemonica_cf(Name, CF)), JSON1),
 
-    (facto(_, via_aerea_dificil(true)),
-    append(JSON1, [via_aerea_dificil=true], JSON2);
-    append(JSON1, [via_aerea_dificil=false], JSON2)),
+    (   facto(PatientID, _, via_aerea_dificil(true))
+    ->  append(JSON1, [via_aerea_dificil=true], JSON2)
+    ;   append(JSON1, [via_aerea_dificil=false], JSON2)
+    ),
 
-    ultimo_rec_processo(ValorRec),
+    ultimo_rec_processo(PatientID, ValorRec),
     append(JSON2, [ultimo_rec_processo=ValorRec], JSONFinal).
 
 % HTTP GET para explicações "como"
 :- http_handler(root(explain), get_explanation_json, []).
 get_explanation_json(Request) :-
-    http_parameters(Request, [id(ID, [integer])]),
-    como_json(ID, JSON),
+    http_parameters(Request, [
+        id(ID, [integer]),
+        patientId(PatientID, [optional('unknown')])
+    ]),
+    %como_json(PatientID, ID, JSON),
     reply_json(JSON).
 
-% HTTP Post para Laringoscopia Direta
+% HTTP POST para Laringoscopia Direta
 :- http_handler(root(laringoscopia), post_laringoscopia, [method(post)]).
 post_laringoscopia(Request) :-
     http_read_json_dict(Request, Dict),
-    laringoscopia(Dict),
+    PatientID = Dict.patientId,
+    laringoscopia(PatientID, Dict),
+    reply_processo_json(PatientID).
 
-    reply_processo_json.
-
-% HTTP Post para Mascara Facial
+% HTTP POST para Máscara Facial
 :- http_handler(root(mascara_facial), post_mascara_facial, [method(post)]).
 post_mascara_facial(Request) :-
     http_read_json_dict(Request, Dict),
-    mascara_facial(Dict),
-
-    reply_processo_json.
+    PatientID = Dict.patientId,
+    mascara_facial(PatientID, Dict),
+    reply_processo_json(PatientID).
